7354e1837cf38933e2c7a09f2b75c40390bf617c
Yamsie <14147009@studentmail.ul.ie>
2017-02-08 19:17:54 +0000

    Testing Version Control

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -3,6 +3,7 @@ package Calculator;
 /**
  * Created by James on 08/02/2017.
  * Controller makes the view and model work together, the two do not interact directly
+ * Adding this to test version control
  */
 public class Calculator {
     public static void main(String [] args) {
