3caf6fb06d65517f3a5e10427039dffb85359b85
Yamsie <14147009@studentmail.ul.ie>
2017-03-20 02:07:49 +0000

    Made changed to Manager GUI

--- src/UI/Views/ManagerView.java
+++ src/UI/Views/ManagerView.java
@@ -2,24 +2,21 @@ package UI.Views;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.*;
 import javax.swing.border.Border;
 
+import BLL.Models.Employees.Employee;
 import UI.Controller.ManagerController;
 /**
  * Created by James on 2017/3/12.
  */
 public class ManagerView extends JFrame implements I_EmployeeView {
-    //private JTextField userName = new JTextField(20);
-    //private JLabel additionLabel1 = new JLabel("USERNAME");
-    //private JTextField passWord = new JTextField(20);
-    //private JLabel additionLabel2 = new JLabel("PASSWORD");
-    //private JButton Button = new JButton("Submit");
-
-    private JButton employeeButton = new JButton("View Orders");
-    private JButton orderButton = new JButton("View Employees");
+    private JButton employeeButton = new JButton("View Employees");
+    private JButton orderButton = new JButton("View Orders");
     private JButton customerButton = new JButton("View Customers");
     private JLabel nameLabel = new JLabel("Logged in as: ");
+    private JLabel displayField = new JLabel();
 
 
     //private JLabel empLabel = new JLabel("View All Orders");
@@ -33,12 +30,11 @@ public class ManagerView extends JFrame implements I_EmployeeView {
         JPanel buttonsPanel = new JPanel();
         JPanel namePanel = new JPanel();
         JPanel displayPanel = new JPanel();
-        JLabel displayField = new JLabel();
         JScrollPane scroller = new JScrollPane(displayField, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setTitle("Manager Window");
-        this.setSize(600, 600);
+        this.setSize(500, 500);
         displayField.setSize(30,50);
         displayField.setText("");
 
@@ -46,12 +42,13 @@ public class ManagerView extends JFrame implements I_EmployeeView {
         buttonsPanel.add(orderButton);
         buttonsPanel.add(customerButton);
         namePanel.add(nameLabel);
+        displayPanel.add(displayField);
         displayPanel.add(scroller);
 
-        setLayout(new BorderLayout());
-        add(buttonsPanel, BorderLayout.WEST);
+        setLayout(new GridLayout(3,3));
         add(namePanel, BorderLayout.NORTH);
-        add(displayPanel, BorderLayout.SOUTH);
+        add(buttonsPanel, BorderLayout.WEST);
+        add(displayPanel, BorderLayout.EAST);
     }
 
     public void employeeListener(ActionListener listenForButton) {
@@ -78,9 +75,9 @@ public class ManagerView extends JFrame implements I_EmployeeView {
 
     }
 
-
-
-
+    public void setDisplayText(String output) {
+        displayField.setText(output);
+    }
     public void displayErrorMessage(String errorMessage) {
         JOptionPane.showMessageDialog(this, errorMessage);
     }
