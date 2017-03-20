6e2e662032bb02e4d0ee4b537e4cbc5e9c649502
jackdeegan <jack.deegan@gmail.com>
2017-03-20 12:25:20 +0000

    Added code to Loader.java

--- src/BLL/Models/Employees/Loader.java
+++ src/BLL/Models/Employees/Loader.java
@@ -1,9 +1,19 @@
 package BLL.Models.Employees;
 
 
-public class Loader extends Employee
+public class Loader implements I_Employee
 {
+	private String loaderStatus;
 	
+	public void Loader(String loaderStatus){
+		this.loaderStatus = loaderStatus
+	}
 	
+	public String getLoaderStatus(){
+		return loaderStatus;
+	}
 	
+	public void setLoaderStatus(String loaderStatus){
+		this.loaderStatus = loaderStatus;
+	}
 }
