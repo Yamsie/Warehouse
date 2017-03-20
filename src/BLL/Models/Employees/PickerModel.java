fa6e71ed7ccc69b69dfdc2ccf9daa673684e9dd7
Lenovo <16006245@studentmail.ul.ie>
2017-03-20 15:56:05 +0000

    Views and  Controller

---
+++ src/BLL/Models/Employees/PickerModel.java
@@ -0,0 +1,29 @@
+package BLL.Models.Employees;
+
+//import DAL Package
+/**
+ * Created by JiasenTian on 2017/3/19.
+ */
+public class PickerModel extends Employee {
+    private String jobTitle;
+    //DBOperater pickerBD = new DBPikccr();
+
+    public PickerModel(String userName, int id, String email){
+        super(userName, id, email);
+        jobTitle = "Picker";
+    }
+
+    public String [] getItemInfo(int ItemID){
+        String [] ItemInfo = new String[7];
+        //ItemInfo = pickerDB.getItemInfo(ItemID);
+        return ItemInfo;
+    }
+
+    public String [] getOrderInfo(int OrderID){
+        String [] OrderInfo = new String[9];
+        //OrderInfo = pickerDB.getOrderInfo(OrderID);
+        return OrderInfo;
+    }
+
+    public String getJobTitle() { return jobTitle;}
+}
