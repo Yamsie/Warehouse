fa81a97ee6b3f9abb42a1d8e89537cd00b74fa7a
ShaneKennyK2 <Shanekenny96@gmail.com>
2017-03-23 19:11:27 +0000

    Updated Item Class

--- src/BLL/Model/Inventory/Item.java
+++ src/BLL/Model/Inventory/Item.java
@@ -1,21 +1,86 @@
+package BLL.Model.Inventory;
+
 public class Item
 {
-	public String itemType;
-	public String manufacturer;
-	public String size;
-	public int itemID;	
-	public int quantity;
-	public double sPrice;
-	public double pPrice;
-	
-	public Item(int itemID, String itemType, String manufacturer, String size, int quantity, double sPrice, double pPrice)
+	private String itemType;
+	private String manufacturer;
+	private String size;
+	private int itemID;
+	private int quantity;
+	private double sPrice;
+	private double pPrice;
+
+	public Item(int itemID, String itemType, String manufacturer, String size, int quantity, double sPrice, double pPrice)	//Item constructor method
 	{
-		this.itemID = itemID;
-		this.itemType = itemType;
-		this.manufacturer = manufacturer;
+
+		this.itemID = itemID;					//"this.itemID" refers to the current itemID
+		this.itemType = itemType;				//that was created when we called this constructor while
+		this.manufacturer = manufacturer;		//the second "itemID" refers to the parameter of the constructor!
 		this.size = size;
 		this.quantity = quantity;
 		this.sPrice = sPrice;
 		this.pPrice = pPrice;
 	}
+
+	@Override
+	public String toString() {
+		return itemID + "," + itemType + "," + manufacturer + "," + size + "," + quantity + "," + sPrice + "," + pPrice;
+	}
+
+	public String getItemType() {
+		return itemType;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public int getItemID() {
+		return itemID;
+	}
+
+	public int quantity() {
+		return quantity;
+	}
+
+	public double sPrice() {
+		return sPrice;
+	}
+
+	public double pPrice() {
+		return pPrice;
+	}
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+    
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public void setsPrice(double sPrice) {
+		this.sPrice = sPrice;
+	}
+
+	public void setpPrice(double pPrice) {
+		this.pPrice = pPrice;
+	}
+
 }
