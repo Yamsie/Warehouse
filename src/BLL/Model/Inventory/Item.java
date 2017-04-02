b5cccf4131f4c3486ffbd26ef881c3b667fd675b
ShaneKennyK2 <Shanekenny96@gmail.com>
2017-04-02 17:17:18 +0100

    Updated Item Class, DatabaseServices, AccessInventoty and all Stocker files!

--- src/BLL/Model/Inventory/Item.java
+++ src/BLL/Model/Inventory/Item.java
@@ -11,13 +11,12 @@ public class Item
 	private double sPrice;
 	private double pPrice;
 
-	public Item(int itemID, String itemType, String manufacturer, String size, double volume, int quantity, double sPrice, double pPrice)
+	public Item(int itemID, String itemType, String manufacturer, String size, int quantity, double sPrice, double pPrice)
 	{
 		this.itemID = itemID;
 		this.itemType = itemType;
 		this.manufacturer = manufacturer;
 		this.size = size;
-		this.volume = volume;
 		this.quantity = quantity;
 		this.sPrice = sPrice;
 		this.pPrice = pPrice;
