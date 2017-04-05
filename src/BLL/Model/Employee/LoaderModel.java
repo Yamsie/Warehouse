85e006a60923980753f16c4ec31f382589dfb0db
jackdeegan <jack.deegan@gmail.com>
2017-04-05 15:40:43 +0100

    Completed Loader code

--- src/BLL/Model/Employee/LoaderModel.java
+++ src/BLL/Model/Employee/LoaderModel.java
@@ -1,24 +1,26 @@
 package BLL.Model.Employee;
+import BLL.Model.Box.BoxFactory;
+import BLL.Model.Box.I_Box;
 import BLL.Model.Employee.I_EmployeeModel;
 
+import BLL.Model.Order.CustomerOrder;
 import DAL.DatabaseService.AccessCountries;
 import DAL.DatabaseService.AccessCustomerOrders;
 import DAL.DatabaseService.DatabaseService;
 
-import java.util.List;
+import javax.swing.*;
 
-public class LoaderModel extends Employee implements I_EmployeeModel
-{
-	//maybe automated?
-	//each country has a specific loading zone
-	//check if order is in loading zone
+public class LoaderModel extends Employee implements I_EmployeeModel {
+    //maybe automated?
+    //each country has a specific loading zone
+    //check if order is in loading zone
 
-	String jobTitle;
-	DatabaseService loaderDB = new AccessCustomerOrders();
-	DatabaseService loaderDBCountry = new AccessCountries();
+    String jobTitle;
+    DatabaseService loaderDB = new AccessCustomerOrders();
+    DatabaseService loaderDBCountry = new AccessCountries();
 
-	public LoaderModel(int id, String userName, String jobTitle, String email) {
-        super( id, userName, jobTitle, email);
+    public LoaderModel(int id, String userName, String jobTitle, String email) {
+        super(id, userName, jobTitle, email);
     }
 
     public String[] showChosenOrder(String chosenOrder) {
@@ -32,35 +34,40 @@ public class LoaderModel extends Employee implements I_EmployeeModel
         String chosenCountryDetails = loaderDBCountry.getCountryData(chosenCountryName);
         chosenCountryInfo = chosenCountryDetails.split(",");
 
-        System.out.println(chosenOrderInfo[4]);
-        System.out.println(chosenOrderInfo[5]);
-        System.out.println(chosenCountryInfo[0]);
-        System.out.println(chosenCountryInfo[1]);
+        if (chosenOrderInfo[4].equals(chosenCountryInfo[0])) {
+            if (!(chosenOrderInfo[5].equals(chosenCountryInfo[1]))) {
 
-        if(chosenOrderInfo[4].equals(chosenCountryInfo[0]))   {
-            if(!(chosenOrderInfo[5].equals(chosenCountryInfo[1])))  {
-                if(chosenOrderInfo[4].equals("Ireland") || chosenOrderInfo[4].equals("UK"))
+                if (chosenOrderInfo[4].equals("Ireland") || chosenOrderInfo[4].equals("UK")) {
                     chosenOrderInfo[5] = "1";
-                else if(chosenOrderInfo[5].equals("France") || chosenOrderInfo[5].equals("Spain") ||
-                        chosenOrderInfo[5].equals("Portugal"))
+                    chosenOrderInfo[6] = "SHIPPED";
+                }
+                else if (chosenOrderInfo[4].equals("France") || chosenOrderInfo[4].equals("Spain") ||
+                        chosenOrderInfo[4].equals("Portugal")) {
                     chosenOrderInfo[5] = "3";
-                else if(chosenOrderInfo[5].equals("Switzerland") ||
-                        chosenOrderInfo[5].equals("Italy") || chosenOrderInfo[5].equals("Austria"))
+                    chosenOrderInfo[6] = "SHIPPED";
+                }
+                else if (chosenOrderInfo[4].equals("Switzerland") ||
+                        chosenOrderInfo[4].equals("Italy") || chosenOrderInfo[4].equals("Austria")) {
                     chosenOrderInfo[5] = "4";
-                else if(chosenOrderInfo[5].equals("Netherlands") || chosenOrderInfo[5].equals("Belgium") ||
-                        chosenOrderInfo[5].equals("Germany") || chosenOrderInfo[5].equals("Denmark"))
+                    chosenOrderInfo[6] = "SHIPPED";
+                }
+                else if (chosenOrderInfo[4].equals("Netherlands") || chosenOrderInfo[4].equals("Belgium") ||
+                        chosenOrderInfo[4].equals("Germany") || chosenOrderInfo[4].equals("Denmark")) {
                     chosenOrderInfo[5] = "5";
-                else if(chosenOrderInfo[5].equals("Norway") || chosenOrderInfo[5].equals("Sweden") ||
-                        chosenOrderInfo[5].equals("Finland") || chosenOrderInfo[5].equals("Iceland"))
+                    chosenOrderInfo[6] = "SHIPPED";
+                }
+                else if (chosenOrderInfo[4].equals("Norway") || chosenOrderInfo[4].equals("Sweden") ||
+                        chosenOrderInfo[4].equals("Finland") || chosenOrderInfo[4].equals("Iceland")) {
                     chosenOrderInfo[5] = "6";
+                    chosenOrderInfo[6] = "SHIPPED";
+                }
             }
+            else
+                chosenOrderInfo[6] = "SHIPPED";
         }
-
-        System.out.println(chosenOrderInfo[4]);
-        System.out.println(chosenOrderInfo[5]);
-        System.out.println(chosenCountryInfo[0]);
-        System.out.println(chosenCountryInfo[1]);
-
+        loaderDB.changeData(chosenOrderInfo);
+        JOptionPane.showMessageDialog(null, "Shipped!");
         return chosenOrderInfo;
     }
 }
+
